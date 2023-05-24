d415150769d5ee1f8b97d34a78af97de2845f2d2
LucaGhironziDanielCastelao <131878356+LucaGhironziDanielCastelao@users.noreply.github.com>
2023-05-24 13:23:49 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -2,8 +2,9 @@ public class Car {
   private String name;
   private Integer speed;
   
-  public Car(String name) {
+  public Car(String name, Integer speed) {
     this.name = name;
+    this.speed = speed;
   }
   public void increaseSpeed() {
     this.speed++;
