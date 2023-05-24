b2156944c0b762d12f686d1cc0ce7bb972784b4f
LucaGhironziDanielCastelao <131878356+LucaGhironziDanielCastelao@users.noreply.github.com>
2023-05-24 11:30:12 +0000

    Fixed menu.java

--- Car.java
+++ Car.java
@@ -1,6 +1,10 @@
 public class Car {
   private String name;
+    public void setName(String name) { this.name = name; }
+    public String getName() { return this.name; }
   private Integer speed;
+    public void setSpeed(Integer speed) { this.speed = speed; }
+    public Integer getSpeed() { return this.speed; }
   
   public Car(String name, Integer speed) {
     this.name = name;
--- Menu.java
+++ Menu.java
@@ -1,22 +1,20 @@
 public class Menu {
   private Car[] cars = null;
   
-  void viewCars() {
-    System.out.println("Cars\nName\tSpeed");
-    if(cars != null) for(Car car : cars) {
-      System.out.println(car.name + "\t" + car.speed);
+  void view() {
+    if(cars == null) System.out.println("No cars to display.\n");
+    else {
+      System.out.println("Cars\nName\tSpeed");
+      for(Car car : cars) System.out.println(car.getName() + "\t" + car.getSpeed());
     }
     System.out.println();
+
+    System.out.println("1 - New car.");
+    System.out.println("2 - Increase car speed.");
+    System.out.println("3 - Decrease car speed.");
   }
   
   public Menu() {
-    viewCars();
-    viewOptions();
+    view();
   }
-  public viewOptions() {
-    System.out.println("1 - New car.")
-    System.out.println("2 - Increase car speed.")
-    System.out.println("3 - Decrease car speed.")
-  }
-  public 
 }
