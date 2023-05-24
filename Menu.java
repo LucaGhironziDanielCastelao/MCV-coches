99a565f389506cdbecc46b6c57991fabb3878c26
LucaGhironziDanielCastelao <131878356+LucaGhironziDanielCastelao@users.noreply.github.com>
2023-05-24 13:23:25 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,17 +1,22 @@
 public class Menu {
-  Car[] cars = null;
+  private Car[] cars = null;
+  
   void viewCars() {
+    System.out.println("Cars\nName\tSpeed");
     if(cars != null) for(Car car : cars) {
       System.out.println(car.name + "\t" + car.speed);
     }
     System.out.println();
   }
+  
   public Menu() {
     viewCars();
     viewOptions();
   }
   public viewOptions() {
     System.out.println("1 - New car.")
-    System.out.println("2 - Change car speed.")
+    System.out.println("2 - Increase car speed.")
+    System.out.println("3 - Decrease car speed.")
   }
+  public 
 }
